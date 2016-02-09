7059447bef532935cadea215080109331e63e162
zaraboy <bagaturcho@abv.bg>
2016-02-09 19:12:21 +0000

    asdasd

---
+++ tango_with_django_project/rango/migrations/0003_category_slug.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rango', '0002_auto_20160206_1739'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='category',
+            name='slug',
+            field=models.SlugField(default=''),
+            preserve_default=False,
+        ),
+    ]
